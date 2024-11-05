b4ec3a06f882597de4bdc261e6aeebb73afbbf27
playdata\playdata2 <yejin3388@naver.com>
2024-11-05 12:48:39 +0900

    Update: 다중선택 버튼 추가!

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ st.title('🐻My First Streamlit App🐻')
 
 st.header('이게 된다고?🤔⚾')
 
-
+multi_select = st.multiselect(
+    label = '좋아하는 과일을 입력하세요.',
+    options = ['사과', '바나나', '파인애플', '키위', '아보카도']
+)
+st.write(f'당신이 좋아하는 과일은 :blue[**{multi_select}**]입니다.')
 
